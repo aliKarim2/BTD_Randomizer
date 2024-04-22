05606a9703b3ecf06c6460c8f0d499f697278214
aliKarim2 <222alikarim@gmail.com>
2024-04-22 04:46:25 -0700

    add basic outputting system

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
 
 class Tower{
     std::string name;
@@ -22,10 +25,34 @@ public:
 
 
 int main(){
+    const int FW = 20;
+
+
+    
+    std::cout << std::left;
+    std::cout << "\n\n";
+
+    //output all of player 1's towers
+    std::cout << "PLAYER 1:\n";
+    std::cout << "Tower 1: " << std::right << std::setw(FW) << "XXXXX" << '\n';
+    std::cout << "Tower 2: " << std::right << std::setw(FW) << "YYYYY" << '\n';
+    std::cout << "Tower 3: " << std::right << std::setw(FW) << "ZZZZZ" << '\n';
+
+    std::cout << std::setw(50) << std::setfill('-') << "" << std::endl;    //line barrier
+    std::cout<< std::setfill(' ') << std::setw(0)  << "";    //line barrier end
+
+    //output all of player 2's towers
+    std::cout << "PLAYER 2:\n";
+    std::cout << "Tower 1: " << std::right << std::setw(FW) << "XXXXX" << '\n';
+    std::cout << "Tower 2: " << std::right << std::setw(FW) << "YYYYY" << '\n';
+    std::cout << "Tower 3: " << std::right << std::setw(FW) << "ZZZZZ" << '\n';
 
 
 
 
+
+    std::cout << std::setw(50) << std::setfill('-') << "" << std::endl;    //line barrier
+    std::cout<< std::setfill(' ') << std::setw(0)  << "";    //line barrier end
 
 
 
